607b346575314fde3fdf1cb0cdf7b813d1dfcb06
hungdzvcllll <fptjun@gmail.com>
2024-09-30 22:19:19 +0700

    hdz

---
+++ src/Calculate.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Calculate {
+    public static void main(String args[]) {
+        double a, b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        System.out.println("a+b=" + (a + b));
+        System.out.println("a-b=" + (a - b));
+        System.out.println("a*b=" + (a * b));
+        System.out.println("a/b=" + (a / b));
+
+    }
+}
